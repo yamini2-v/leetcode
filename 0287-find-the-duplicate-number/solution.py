d117f7914c30cd4136442708fa01be5fb05a8620
yamini2-v <yaminivanguri42@gmail.com>
2026-06-04 15:31:29 +0000

    Sync LeetCode submission Runtime - 18 ms (92.05%), Memory - 37.2 MB (7.74%)

---
+++ 0287-find-the-duplicate-number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        d = dict()
+        for num in nums:
+            if num in d:
+                return num
+            else:
+                d[num] = 1    

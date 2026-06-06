0467cf33a6c5537b0e7facadc513c7a58273020c
yamini2-v <yaminivanguri42@gmail.com>
2026-06-06 07:08:30 +0000

    Sync LeetCode submission Runtime - 3 ms (66.28%), Memory - 19.4 MB (83.88%)

---
+++ 2714-left-and-right-sum-differences/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        l, r = 0, sum(nums)
+        ans = []
+        for x in nums:
+            r -= x
+            ans.append(abs(l - r))
+            l += x
+        return ans
+
+            
+        

bbc6a066b46bfacd30e48dd5c3abd020ad16681e
yamini2-v <yaminivanguri42@gmail.com>
2026-06-13 16:44:12 +0000

    Sync LeetCode submission Runtime - 8 ms (21.33%), Memory - 21.6 MB (18.66%)

---
+++ 0136-single-number/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        for k,v in count.items():
+            if v == 1:
+                return k
+        

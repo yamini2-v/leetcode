934015c1a9635922321401f4c55bf4aa0300e759
yamini2-v <yaminivanguri42@gmail.com>
2026-06-16 06:32:45 +0000

    Sync LeetCode submission Runtime - 3 ms (52.84%), Memory - 19.3 MB (91.04%)

---
+++ 2048-build-array-from-permutation/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        l = []
+        for num in nums:
+            s = nums[num]
+            l.append(s)
+        return l
+        

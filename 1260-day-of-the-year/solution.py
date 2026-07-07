baa80e8d863faf8b8d0826ef7d1f7de687ba84be
yamini2-v <yaminivanguri42@gmail.com>
2026-07-07 05:12:07 +0000

    Sync LeetCode submission Runtime - 11 ms (59.48%), Memory - 19.3 MB (26.49%)

---
+++ 1260-day-of-the-year/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        def isleapyear(year:int) -> bool:
+            if (year%4==0 and year%100!=0) or year%400==0:
+                return True
+            else:
+                return False
+
+        year=int(date[:4])
+        month=int(date[5:7])
+        day=int(date[8:])
+        days=[31,29 if isleapyear(year) else 28,31,30,31,30,31,31,30,31,30,31]
+        return sum(days[:month-1])+day
+        
+        

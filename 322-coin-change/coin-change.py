67443fdbfaaddab3d90d5a3bf4d2361d5823504a
yamini2-v <yaminivanguri42@gmail.com>
2026-08-04 14:06:29 +0530

    Time: 479 ms (68.88%) | Memory: 19.7 MB (38.60%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,17 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+       coins.sort()
+       dp = [0]*(amount+1)
+       for i in range(1,amount+1):
+          minn=float('inf')
+          for coin in coins:
+               diff = i-coin
+               if diff<0:
+                   break
+               minn = min(minn,dp[diff]+1)
+          dp[i]=minn
+       if dp[amount]<float('inf'):
+             return dp[amount]
+       else:
+            return -1
+        
